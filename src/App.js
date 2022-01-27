2f196c848dd1a40973b1b8dbde9f07d04c23a4b9
Glebkas <baran123456@gmail.com>
2022-01-27 17:33:17 +0200

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Notfound from "./pages/Notfound";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout></Layout>}>
+            <Route index path="/" element={<Homepage></Homepage>}></Route>
+            <Route path="portfolio" element={<Portfolio></Portfolio>}></Route>
+            <Route path="contact" element={<Contact></Contact>}></Route>
+            <Route path="*" element={<Notfound></Notfound>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// import { useState } from 'react';
+import Nav from "./Navbar";
+function Header() {
+  return (
+    <div className="header">
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default Header;
