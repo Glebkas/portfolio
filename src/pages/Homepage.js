fab224ebdb0e3ad3e75720c5f3c45171eee5ca6f
Glebkas <baran123456@gmail.com>
2022-01-30 22:55:10 +0200

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,19 @@
+import data from "../utils/data";
+
 function Homepage() {
   return (
-    <div>
-      <h2>HomePage</h2>
+    <div className="homepage">
+      <section className="homepage-top">
+        <div className="homepage-top__image">
+          <h2 className="homepage-top__title">
+            {data.info.title}
+            <div className="homepage-top__button">
+              <div className="homepage-top__button-icon"></div>
+              <p className="homepage-top__button-caption">About Me</p>
+            </div>
+          </h2>
+        </div>
+      </section>
     </div>
   );
 }
