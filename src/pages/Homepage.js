6446fdd16e13154c160d0a206bf100e767bcd052
Glebkas <baran123456@gmail.com>
2022-01-31 22:52:33 +0200

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,5 @@
 import data from "../utils/data";
-
+import { Link } from "react-router-dom";
 function Homepage() {
   return (
     <div className="homepage">
@@ -12,6 +12,16 @@ function Homepage() {
               <p className="homepage-top__button-caption">About Me</p>
             </div>
           </h2>
+        </div>
+      </section>
+      <section className="about">
+        <div className="about__image" />
+        <div className="about__description-container">
+          <h2 className="about__title">About Me</h2>
+          <p className="about__description">{data.info.aboutMe}</p>
+          <Link className="about__button" to="/portfolio">
+            Portfolio
+          </Link>
         </div>
       </section>
     </div>
