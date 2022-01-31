fac1a17049b17e3bcfe4954e248d1c007038b7d3
Glebkas <baran123456@gmail.com>
2022-01-31 22:52:30 +0200

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,7 @@
 const data = {
   info: {
     title: "Hey, I’m Guy Gleb Kasner and I’m a Front End Developer",
+    image: "../../../public/images/profile-image.jpg",
     aboutMe:
       "I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in London, UK, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work. ",
   },
