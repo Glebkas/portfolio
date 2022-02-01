e2bef1be63b21a29dbdc331ca294d039f2adfe61
Glebkas <baran123456@gmail.com>
2022-02-01 23:19:19 +0200

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,27 +1,40 @@
 import data from "../utils/data";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
 function Homepage() {
+  let navigate = useNavigate();
+  const handleAboutbuttonClick = () => {
+    navigate("/portfolio");
+  };
+
   return (
     <div className="homepage">
       <section className="homepage-top">
         <div className="homepage-top__image">
-          <h2 className="homepage-top__title">
-            {data.info.title}
-            <div className="homepage-top__button">
-              <div className="homepage-top__button-icon"></div>
-              <p className="homepage-top__button-caption">About Me</p>
-            </div>
-          </h2>
+          <div className="homepage-top__title-container">
+            <h2 className="homepage-top__title">{data.info.title}</h2>
+            <HashLink style={{ textDecoration: "none" }} smooth to="/#about">
+              <div className="homepage-top__button">
+                <div className="homepage-top__button-icon"></div>
+                <p className="homepage-top__button-caption">About Me</p>;
+              </div>
+            </HashLink>
+          </div>
         </div>
       </section>
-      <section className="about">
+      <section id="about" className="about">
         <div className="about__image" />
         <div className="about__description-container">
           <h2 className="about__title">About Me</h2>
           <p className="about__description">{data.info.aboutMe}</p>
-          <Link className="about__button" to="/portfolio">
-            Portfolio
-          </Link>
+          <button
+            onClick={handleAboutbuttonClick}
+            className="about__button"
+            to="/portfolio"
+          >
+            Go to Portfolio
+          </button>
         </div>
       </section>
     </div>
