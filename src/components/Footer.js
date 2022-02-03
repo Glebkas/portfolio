42288a90f909503cec537127e5340b352915b6d9
Glebkas <baran123456@gmail.com>
2022-02-04 00:53:59 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import Nav from "./Nav";
 function Footer() {
-  const setActive = ({ isActive }) =>
+  const setFooterNavActive = ({ isActive }) =>
     isActive ? "nav__link nav__link_active" : "nav__link nav__link_type_footer";
   return (
     <div className="footer">
       <div className="footer__container">
         <div className="footer__logo"></div>
-        <Nav setActive={setActive}></Nav>
+        <Nav setActive={setFooterNavActive}></Nav>
         <div className="footer__icons">
           <i className="footer__icons_type_github" />
           <i className="footer__icons_type_linkedin" />
