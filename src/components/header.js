24cabdf08e8b640fd67bba18f9167e3e77319944
Glebkas <baran123456@gmail.com>
2022-02-04 00:54:05 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import Nav from "./Nav";
 function Header() {
-  const setActive = ({ isActive }) =>
+  const setHeaderNavActive = ({ isActive }) =>
     isActive ? "nav__link nav__link_active" : "nav__link";
 
   return (
     <header className="header">
       <div className="header__logo"></div>
-      <Nav setActive={setActive}></Nav>
+      <Nav setActive={setHeaderNavActive}></Nav>
     </header>
   );
 }
