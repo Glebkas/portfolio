8a8c974da97ba6ec841b8d1a7746aaf9bb633e67
Glebkas <baran123456@gmail.com>
2022-02-05 23:43:26 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,37 @@
 import Nav from "./Nav";
+import data from "../utils/data";
 function Footer() {
   const setFooterNavActive = ({ isActive }) =>
     isActive ? "nav__link nav__link_active" : "nav__link nav__link_type_footer";
+
+  const handleIconClick = (e) => {
+    window.open(e.target.id, "_blank");
+    console.log(e.target.id);
+  };
+
   return (
     <div className="footer">
       <div className="footer__container">
         <div className="footer__logo"></div>
-        <Nav setActive={setFooterNavActive}></Nav>
-        <div className="footer__icons">
-          <i className="footer__icons_type_github" />
-          <i className="footer__icons_type_linkedin" />
-          <i className="footer__icons_type_facebook" />
+        <div className="footer__menu-icons-container">
+          <Nav classType={`nav nav_type_footer`} setActive={setFooterNavActive}></Nav>
+          <div className="footer__icons">
+            <i
+              id={data.iconLinks.github}
+              onClick={handleIconClick}
+              className="footer__icons_type_github"
+            />
+            <i
+              id={data.iconLinks.linkedin}
+              onClick={handleIconClick}
+              className="footer__icons_type_linkedin"
+            />
+            <i
+              id={data.iconLinks.facebook}
+              onClick={handleIconClick}
+              className="footer__icons_type_facebook"
+            />
+          </div>
         </div>
       </div>
     </div>
