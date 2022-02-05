39ac7c0cb48850fb88827554b771aefe6e964fc3
Glebkas <baran123456@gmail.com>
2022-02-05 23:43:29 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,25 @@
 import Nav from "./Nav";
-function Header() {
+import { Link } from "react-router-dom";
+function Header(props) {
   const setHeaderNavActive = ({ isActive }) =>
     isActive ? "nav__link nav__link_active" : "nav__link";
 
   return (
     <header className="header">
-      <div className="header__logo"></div>
-      <Nav setActive={setHeaderNavActive}></Nav>
+      <Link to="/" className="header__logo"></Link>
+      <button
+        onClick={props.handleHumburgerClick}
+        className={
+          props.open
+            ? "header__menu-hamburger header__menu-hamburger_close"
+            : "header__menu-hamburger"
+        }
+      ></button>
+      <Nav
+        handleNavLinkClick={props.handleNavLinkClick}
+        classType={props.open ? `nav nav_type_mobile` : `nav nav_type_header`}
+        setActive={setHeaderNavActive}
+      ></Nav>
     </header>
   );
 }
