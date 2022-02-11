9d3adae0e529ed7bee1e7b894655f2f8b30264e7
Glebkas <baran123456@gmail.com>
2022-02-11 20:04:09 +0200

    Create ProjectOverview.js

---
+++ src/components/ProjectOverview.js
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+function ProjectOverview(props) {
+  let navigateToProject = useNavigate();
+  const handleViewProjectbuttonClick = () => {
+    navigateToProject(`/projects/${props.title}`);
+  };
+
+  return (
+    <li id={props.id} className="project-overview">
+      <div className="project-overview__image" />
+      <div className="project-overview__description-container">
+        <h2 className="project-overview__title">{props.title}</h2>
+        <p className="project-overview__description">{props.description}</p>
+        <button
+          onClick={handleViewProjectbuttonClick}
+          className="project-overview__button"
+        >
+          View Project
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default ProjectOverview;
