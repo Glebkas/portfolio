db91f221b6a31e2b5cdb036bf3ea953305a6be41
Glebkas <baran123456@gmail.com>
2022-02-11 20:03:55 +0200

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,12 +3,54 @@ const data = {
     title: "Hey, I’m Guy Gleb Kasner and I’m a Front End Developer",
     image: "../../../public/images/profile-image.jpg",
     aboutMe:
-      "I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in London, UK, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work. ",
+      "I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in Israel, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work. ",
   },
   iconLinks: {
     github: "https://github.com/Glebkas/",
     linkedin: "https://www.linkedin.com/in/guy-gleb-kasner-2b8b7a22a/",
     facebook: "https://www.facebook.com/lonlyvisitor/",
   },
+  projects: [
+    {
+      id: 1,
+      title: "Tip-Calculator",
+      description:
+        "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider. ",
+      screenShotSmall: "screenShotSmall",
+      projectBackground: "blablablablablablablablablablablablablablablabl",
+      liveSite: "livesite",
+      github: "github",
+    },
+    {
+      id: 2,
+      title: "Planets",
+      description:
+        "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
+      screenShotSmall: "screenShotSmall",
+      projectBackground: "blablablablablablablablablablablablablablablabl",
+      liveSite: "livesite",
+      github: "github",
+    },
+    {
+      id: 3,
+      title: "Places",
+      description:
+        "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ",
+      screenShotSmall: "screenShotSmall",
+      projectBackground: "blablablablablablablablablablablablablablablabl",
+      liveSite: "livesite",
+      github: "github",
+    },
+    {
+      id: 4,
+      title: "Portfolio",
+      description:
+        "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ",
+      screenShotSmall: "screenShotSmall",
+      projectBackground: "blablablablablablablablablablablablablablablabl",
+      liveSite: "livesite",
+      github: "github",
+    },
+  ],
 };
 export default data;
