ab4916f3eb2088dc74f1f206bb7c081752247921
Glebkas <baran123456@gmail.com>
2022-02-15 21:44:30 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,19 @@
- 
+function About(props) {
+  return (
+    <section id="about" className="about">
+      <div className="about__image" />
+      <div className="about__description-container">
+        <h2 className="about__title">About Me</h2>
+        <p className="about__description">{props.data.info.aboutMe}</p>
+        <button
+          onClick={props.handleAboutbuttonClick}
+          className="about__button"
+        >
+          Go to Portfolio
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default About;
