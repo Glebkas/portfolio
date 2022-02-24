c09f5074625d36fd23080ef55423cd9caaab68b8
Glebkas <baran123456@gmail.com>
2022-02-24 18:01:10 +0200

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -14,40 +14,56 @@ const data = {
     {
       id: 1,
       title: "Tip-Calculator",
+      image: "images/splitter-overview.jpg",
+      image_header: "../images/splitter-header.jpg",
+      methods: "Interaction Design / Front End Development HTML / CSS / JS",
       description:
         "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider. ",
       screenShotSmall: "screenShotSmall",
-      projectBackground: "blablablablablablablablablablablablablablablabl",
+      project_background:
+        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
       liveSite: "livesite",
       github: "github",
     },
     {
       id: 2,
       title: "Planets",
+      image: "images/planets-overview.jpg",
+      image_header: "../images/planets-header.jpg",
+      methods: "Interaction Design / Front End Development HTML / CSS / JS",
       description:
         "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
       screenShotSmall: "screenShotSmall",
-      projectBackground: "blablablablablablablablablablablablablablablabl",
+      project_background:
+        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
       liveSite: "livesite",
       github: "github",
     },
     {
       id: 3,
       title: "Places",
+      image: "images/places-overview.jpg",
+      image_header: "../images/places-header.jpg",
+      methods: "Interaction Design / Front End Development HTML / CSS / JS",
       description:
         "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ",
       screenShotSmall: "screenShotSmall",
-      projectBackground: "blablablablablablablablablablablablablablablabl",
+      project_background:
+        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
       liveSite: "livesite",
       github: "github",
     },
     {
       id: 4,
       title: "Portfolio",
+      image: "images/portfolio-overview.jpg",
+      image_header: "../images/portfolio-header.jpg",
+      methods: "Interaction Design / Front End Development HTML / CSS / JS",
       description:
         "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ",
       screenShotSmall: "screenShotSmall",
-      projectBackground: "blablablablablablablablablablablablablablablabl",
+      project_background:
+        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
       liveSite: "livesite",
       github: "github",
     },
