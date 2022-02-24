141d106e341e3e50b33d64164d7cf468478ae7cc
Glebkas <baran123456@gmail.com>
2022-02-24 18:01:31 +0200

    Update ProjectOverview.js

--- src/components/ProjectOverview.js
+++ src/components/ProjectOverview.js
@@ -4,12 +4,11 @@ function ProjectOverview(props) {
   let navigateToProject = useNavigate();
   const handleViewProjectbuttonClick = () => {
     navigateToProject(`/portfolio/${props.id}`);
-    console.log(props.key);
   };
 
   return (
     <li id={props.id} className="project-overview">
-      <div className="project-overview__image" />
+      <img src={props.image} alt={props.title} className="project-overview__image" />
       <div className="project-overview__description-container">
         <h2 className="project-overview__title">{props.title}</h2>
         <p className="project-overview__description">{props.description}</p>
