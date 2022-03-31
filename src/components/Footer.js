74effe26f703b9db1e9ee834f8a7512f687cf9d0
Glebkas <baran123456@gmail.com>
2022-03-31 22:03:25 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,27 @@
-import Nav from "./Nav";
-import Icons from "./Icons";
-import { Link } from "react-router-dom";
-function Footer() {
-  const setFooterNavActive = ({ isActive }) =>
-    isActive ? "nav__link nav__link_active" : "nav__link nav__link_type_footer";
+import Nav from './Nav';
+import Icons from './Icons';
+import { Link } from 'react-router-dom';
+function Footer(props) {
+    const setFooterNavActive = ({ isActive }) =>
+        isActive
+            ? 'nav__link nav__link_active'
+            : 'nav__link nav__link_type_footer';
 
-  return (
-    <div className="footer">
-      <div className="footer__container">
-        <Link to="/" className="footer__logo"></Link>
-        <div className="footer__menu-icons-container">
-          <Nav
-            classType={`nav nav_type_footer`}
-            setActive={setFooterNavActive}
-          ></Nav>
-          <Icons />
+    return (
+        <div className='footer'>
+            <div className='footer__container'>
+                <Link to='/' className='footer__logo'></Link>
+                <div className='footer__menu-icons-container'>
+                    <Nav
+                        handleNavLinkClick={props.handleNavLinkClick}
+                        classType={`nav nav_type_footer`}
+                        setActive={setFooterNavActive}
+                    ></Nav>
+                    <Icons />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Footer;
