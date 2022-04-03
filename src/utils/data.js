b241d7ac5a339f2229fde0014c4daf5fe223c4a0
Glebkas <baran123456@gmail.com>
2022-04-03 10:15:58 +0300

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,78 +1,93 @@
 const data = {
-  info: {
-    title: "Hey, I’m Guy Gleb Kasner and I’m a Front End Developer",
-    image: "../../../public/images/profile-image.jpg",
-    aboutMe:
-      "I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in Israel, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work. ",
-  },
-  iconLinks: {
-    github: "https://github.com/Glebkas/",
-    linkedin: "https://www.linkedin.com/in/guy-gleb-kasner-2b8b7a22a/",
-    facebook: "https://www.facebook.com/lonlyvisitor/",
-  },
-  projects: [
-    {
-      id: 1,
-      title: "Tip-Calculator",
-      image: "images/splitter-overview.jpg",
-      image_header: "../images/splitter-header.jpg",
-      methods: "Interaction Design / Front End Development",
-      progleng: "HTML / CSS / JS",
-      description:
-        "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider. ",
-      screenShotSmall: "screenShotSmall",
-      project_background:
-        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
-      liveSite: "livesite",
-      github: "github",
+    info: {
+        title: 'Hey, I’m Guy Gleb Kasner and I’m a Front End Developer',
+        image: '../../../public/images/profile-image.jpg',
+        aboutMe:
+            'I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in Israel, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work. ',
     },
-    {
-      id: 2,
-      title: "Planets",
-      image: "images/planets-overview.jpg",
-      image_header: "../images/planets-header.jpg",
-      methods: "Interaction Design / Front End Development",
-      progleng: "HTML / CSS / JS",
-      description:
-        "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
-      screenShotSmall: "screenShotSmall",
-      project_background:
-        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
-      liveSite: "livesite",
-      github: "github",
+    iconLinks: {
+        github: 'https://github.com/Glebkas/',
+        linkedin: 'https://www.linkedin.com/in/guy-gleb-kasner-2b8b7a22a/',
+        facebook: 'https://www.facebook.com/lonlyvisitor/',
     },
-    {
-      id: 3,
-      title: "Places",
-      image: "images/places-overview.jpg",
-      image_header: "../images/places-header.jpg",
-      methods: "Interaction Design / Front End Development HTML / CSS / JS",
-      progleng: "HTML / CSS / JS",
-      description:
-        "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ",
-      screenShotSmall: "screenShotSmall",
-      project_background:
-        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
-      liveSite: "livesite",
-      github: "github",
-    },
-    {
-      id: 4,
-      title: "Portfolio",
-      image: "images/portfolio-overview.jpg",
-      image_header: "../images/portfolio-header.jpg",
-      methods: "Interaction Design / Front End Development",
-      progleng: "HTML / CSS / JS",
-      description:
-        "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ",
-      screenShotSmall: "screenShotSmall",
-      project_background:
-        "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
-      liveSite: "livesite",
-      github: "github",
-    },
-  ],
-  contact:
-    "I’d love to hear about what you’re working on and how I could help I’m currently looking for a new role and am open to a wide range of opportunities. My preference would be to find a position in a company in London. But I’m also happy to hear about opportunites that don’t fit that description. I’m a hard-working and positive person who will always approach each task with a sense of purpose and attention to detail. Please do feel free to check out my online profiles below andget in touch using the form.",
+    projects: [
+        {
+            id: 1,
+            title: 'Tip-Calculator',
+            image: 'images/splitter-overview.jpg',
+            image_header: '../images/splitter-header.jpg',
+            methods: 'Interaction Design / Front End Development',
+            progleng: 'HTML / CSS / JS',
+            description:
+                'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider. ',
+            screenShotSmall: 'screenShotSmall',
+            project_background:
+                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+            liveSite: 'https://glebkas.github.io/tip-calculator-app-main/',
+            github: 'https://github.com/Glebkas/tip-calculator-app-main',
+        },
+        {
+            id: 2,
+            title: 'Planets',
+            image: 'images/planets-overview.jpg',
+            image_header: '../images/planets-header.jpg',
+            methods: 'Interaction Design / Front End Development',
+            progleng: 'HTML / CSS / JS',
+            description:
+                'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.',
+            screenShotSmall: 'screenShotSmall',
+            project_background:
+                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+            liveSite: 'https://glebkas.github.io/planets/',
+            github: 'https://github.com/Glebkas/planets',
+        },
+        {
+            id: 3,
+            title: 'Places',
+            image: 'images/places-overview.jpg',
+            image_header: '../images/places-header.jpg',
+            methods: 'Interaction Design / Front End Development',
+            progleng: 'HTML / CSS / JS',
+            description:
+                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+            screenShotSmall: 'screenShotSmall',
+            project_background:
+                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+            liveSite: 'https://glebkas.students.nomoreparties.sbs/signin',
+            github: 'https://github.com/Glebkas/react-around-api-full',
+        },
+        {
+            id: 4,
+            title: 'Portfolio',
+            image: 'images/portfolio-overview.jpg',
+            image_header: '../images/portfolio-header.jpg',
+            methods: 'Interaction Design / Front End Development',
+            progleng: 'HTML / CSS / JS',
+            description:
+                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+            screenShotSmall: 'screenShotSmall',
+            project_background:
+                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+            liveSite: 'livesite',
+            github: 'https://github.com/Glebkas/Portfolio',
+        },
+        {
+            id: 5,
+            title: 'Awesome Container Company',
+            image: 'images/awesome-overview.jpg',
+            image_header: '../images/awesome-header.jpg',
+            methods: 'Interaction Design / Front End Development',
+            progleng: 'HTML / CSS / JS',
+            description:
+                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+            screenShotSmall: 'screenShotSmall',
+            project_background:
+                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+            liveSite: 'https://glebkas.github.io/awesome-container-company/',
+            github: 'https://github.com/Glebkas/awesome-container-company',
+        },
+    ],
+    contact:
+        'I’d love to hear about what you’re working on and how I could help I’m currently looking for a new role and am open to a wide range of opportunities. My preference would be to find a position in a company in London. But I’m also happy to hear about opportunites that don’t fit that description. I’m a hard-working and positive person who will always approach each task with a sense of purpose and attention to detail. Please do feel free to check out my online profiles below andget in touch using the form.',
 };
 export default data;
