883002457b0c012023c647a989b25f9d1e293b4b
Glebkas <baran123456@gmail.com>
2022-04-19 22:40:00 +0300

    Update Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,26 +1,28 @@
-import ProjectOverview from "../components/ProjectOverview";
-import data from "../utils/data";
-import Contact from "../components/Contact";
+import ProjectOverview from '../components/ProjectOverview';
+import data from '../utils/data';
+import Contact from '../components/Contact';
 
 function Portfolio() {
-  return (
-    <>
-      <ul className="portfolio">
-        {data.projects.map((project) => (
-          <ProjectOverview
-            key={project.id}
-            id={project.id}
-            image={project.image}
-            project={project}
-            title={project.title}
-            description={project.description}
-            screenShotSmall={project.screenShotSmall}
-          />
-        ))}
-      </ul>
-      <Contact></Contact>
-    </>
-  );
+    return (
+        <>
+            <ul className='portfolio'>
+                {data.projects.map((project) => (
+                    <ProjectOverview
+                        key={project.id}
+                        id={project.id}
+                        image={project.image}
+                        project={project}
+                        title={project.title}
+                        description={project.description}
+                        screenShotSmall={project.screenShotSmall}
+                    />
+                ))}
+            </ul>
+            <Contact></Contact>
+        </>
+    );
 }
 
 export default Portfolio;
+
+
