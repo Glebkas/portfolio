36ebce82c4cdd04b6cb508580d776a342538e8a1
Glebkas <baran123456@gmail.com>
2022-04-26 15:43:49 +0300

    Update ProjectOverview.js

--- src/components/ProjectOverview.js
+++ src/components/ProjectOverview.js
@@ -1,26 +1,30 @@
-import { useNavigate } from "react-router-dom";
-
 function ProjectOverview(props) {
-  let navigateToProject = useNavigate();
-  const handleViewProjectbuttonClick = () => {
-    navigateToProject(`/portfolio/${props.id}`);
-  };
+    const handleViewProjectbuttonClick = () => {
+        props.navigateToProject(`/portfolio/${props.id}`);
+        props.scrollToTop();
+    };
 
-  return (
-    <li id={props.id} className="project-overview">
-      <img src={props.image} alt={props.title} className="project-overview__image" />
-      <div className="project-overview__description-container">
-        <h2 className="project-overview__title">{props.title}</h2>
-        <p className="project-overview__description">{props.description}</p>
-        <button
-          onClick={handleViewProjectbuttonClick}
-          className="project-overview__button"
-        >
-          View Project
-        </button>
-      </div>
-    </li>
-  );
+    return (
+        <li id={props.id} className='project-overview'>
+            <img
+                src={props.image}
+                alt={props.title}
+                className='project-overview__image'
+            />
+            <div className='project-overview__description-container'>
+                <h2 className='project-overview__title'>{props.title}</h2>
+                <p className='project-overview__description'>
+                    {props.description}
+                </p>
+                <button
+                    onClick={handleViewProjectbuttonClick}
+                    className='project-overview__button'
+                >
+                    View Project
+                </button>
+            </div>
+        </li>
+    );
 }
 
 export default ProjectOverview;
