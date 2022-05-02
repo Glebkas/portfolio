4482ca2d9ca3c01be50bf2f9d42b08841fe808fe
Glebkas <baran123456@gmail.com>
2022-05-02 21:55:51 +0300

    data update

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,7 +3,7 @@ const data = {
         title: 'Hey, I’m Guy Gleb Kasner and I’m a Front End Developer',
         image: '../../../public/images/profile-image.jpg',
         aboutMe:
-            'I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in Israel, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work. ',
+            "I'm a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices, and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I'm based in Israel, but I'm happy working remotely and have experience working remotely. When I'm not coding, you'll find me taking photos outdoors. I'd love you to check out my work. ",
     },
     iconLinks: {
         github: 'https://github.com/Glebkas/',
@@ -13,18 +13,18 @@ const data = {
     projects: [
         {
             id: 1,
-            title: 'Tip-Calculator',
-            image: 'images/splitter-overview.jpg',
-            image_header: '../images/splitter-header.jpg',
+            title: 'Awesome Container Company',
+            image: 'images/awesome-overview.jpg',
+            image_header: '../images/awesome-header.jpg',
             methods: 'Interaction Design / Front End Development',
             progleng: 'HTML / CSS / JS',
             description:
-                'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider. ',
+                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
             screenShotSmall: 'screenShotSmall',
             project_background:
                 'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
-            liveSite: 'https://glebkas.github.io/tip-calculator-app-main/',
-            github: 'https://github.com/Glebkas/tip-calculator-app-main',
+            liveSite: 'https://glebkas.github.io/awesome-container-company/',
+            github: 'https://github.com/Glebkas/awesome-container-company',
         },
         {
             id: 2,
@@ -58,6 +58,21 @@ const data = {
         },
         {
             id: 4,
+            title: 'Countries of the world',
+            image: 'images/countries_of_the_world-overview.jpg',
+            image_header: '../images/countries_of_the_world-header.jpg',
+            methods: 'Interaction Design / Front End Development',
+            progleng: 'HTML / CSS / JS',
+            description:
+                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+            screenShotSmall: 'screenShotSmall',
+            project_background:
+                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+            liveSite: 'https://glebkas.github.io/countries/',
+            github: 'https://github.com/Glebkas/countries',
+        },
+        {
+            id: 5,
             title: 'Portfolio',
             image: 'images/portfolio-overview.jpg',
             image_header: '../images/portfolio-header.jpg',
@@ -72,19 +87,19 @@ const data = {
             github: 'https://github.com/Glebkas/Portfolio',
         },
         {
-            id: 5,
-            title: 'Awesome Container Company',
-            image: 'images/awesome-overview.jpg',
-            image_header: '../images/awesome-header.jpg',
+            id: 6,
+            title: 'Tip-Calculator',
+            image: 'images/splitter-overview.jpg',
+            image_header: '../images/splitter-header.jpg',
             methods: 'Interaction Design / Front End Development',
             progleng: 'HTML / CSS / JS',
             description:
-                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+                'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider. ',
             screenShotSmall: 'screenShotSmall',
             project_background:
                 'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
-            liveSite: 'https://glebkas.github.io/awesome-container-company/',
-            github: 'https://github.com/Glebkas/awesome-container-company',
+            liveSite: 'https://glebkas.github.io/tip-calculator-app-main/',
+            github: 'https://github.com/Glebkas/tip-calculator-app-main',
         },
     ],
     contact:
