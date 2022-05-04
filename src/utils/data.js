d3bfb92cd8cd92f1c6dabb2e10a9b3a053f3c91a
Glebkas <baran123456@gmail.com>
2022-05-04 12:03:56 +0300

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,7 +3,7 @@ const data = {
         title: 'Hey, I’m Guy Gleb Kasner and I’m a Front End Developer',
         image: '../../../public/images/profile-image.jpg',
         aboutMe:
-            "I'm a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices, and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I'm based in Israel, but I'm happy working remotely and have experience working remotely. When I'm not coding, you'll find me taking photos outdoors. I'd love you to check out my work. ",
+            "I'm a junior Full Stack developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices, and writing clean JavaScript. I mostly use React when writing JavaScript code, but I can adapt to whatever tools are required. I'm based in Israel, but I'm happy working remotely and have experience working remotely. When I'm not coding, you'll find me taking photos outdoors. I'd love you to check out my work.",
     },
     iconLinks: {
         github: 'https://github.com/Glebkas/',
@@ -17,12 +17,14 @@ const data = {
             image: 'images/awesome-overview.jpg',
             image_header: '../images/awesome-header.jpg',
             methods: 'Interaction Design / Front End Development',
-            progleng: 'HTML / CSS / JS',
+            progleng: 'HTML / CSS / JS / React',
             description:
-                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+                'This project required me to build a fully responsive landing page for "Awsome Container Company" to the designs provided via Figma and created using React implementing CSS BEM methodology for styling.',
             screenShotSmall: 'screenShotSmall',
             project_background:
-                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+                'Awsome Container Company is a Singapore-based start-up that wants to revolutionize food delivery by eliminating single-use containers and implementing an end-to-end solution for reusable containers.',
+            dev_with:
+                'React | Framer-Motion | React-Compare-Slider | React-Hook-Form ',
             liveSite: 'https://glebkas.github.io/awesome-container-company/',
             github: 'https://github.com/Glebkas/awesome-container-company',
         },
@@ -32,12 +34,14 @@ const data = {
             image: 'images/planets-overview.jpg',
             image_header: '../images/planets-header.jpg',
             methods: 'Interaction Design / Front End Development',
-            progleng: 'HTML / CSS / JS',
+            progleng: 'HTML / CSS / JS / React',
             description:
-                'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.',
+                'A fully responsive SPA planets fact site project lets you learn a bit more about the solar system planets and some key facts; you canview each planets page and toggle between the "Overview", "Internal Structure", and "Surface Geology" tabs.',
             screenShotSmall: 'screenShotSmall',
             project_background:
-                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+                'There are quite a few planetary systems like ours in the universe, with planets orbiting a star (Sun). Our planetary system is named the "solar system" because our Sun is named Sol, after the Latin word for Sun, "Solis" and anything related to the Sun we call "solar. this Spa allows you to learn and take a closer look on our planetary system.',
+            dev_with:
+                'Semantic HTML5 markup | CSS custom properties | BEM | Flexbox | CSS Grid | React ',
             liveSite: 'https://glebkas.github.io/planets/',
             github: 'https://github.com/Glebkas/planets',
         },
@@ -46,14 +50,15 @@ const data = {
             title: 'Places',
             image: 'images/places-overview.jpg',
             image_header: '../images/places-header.jpg',
-            methods: 'Interaction Design / Front End Development',
-            progleng: 'HTML / CSS / JS',
+            methods: 'Interaction Design / Full Stack Development',
+            progleng: 'React / Node.js / Express.js / MongoDB',
             description:
-                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+                'An Instagram-like app allows users to register, share their photos, like photos, delete them, update user information and change their profile image. ',
             screenShotSmall: 'screenShotSmall',
-            project_background:
-                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
-            liveSite: 'https://glebkas.students.nomoreparties.sbs/signin',
+            project_background: '',
+            dev_with:
+                'Semantic HTML5 markup | CSS custom properties | Flexbox | CSS Grid | BEM | JavaScript | React | Node.js | Express.js | MongoDB',
+            liveSite: 'https://glebkas.students.nomoreparties.sbs/',
             github: 'https://github.com/Glebkas/react-around-api-full',
         },
         {
@@ -64,10 +69,11 @@ const data = {
             methods: 'Interaction Design / Front End Development',
             progleng: 'HTML / CSS / JS',
             description:
-                'This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation. ',
+                'A fully responsive SPA Countries fact site project lets you learn a bit more about the countries of the world and some key facts; you can view each countrys page and see the key facts about each country. You can also search for a specific country or filter countries by region and toggle between light and dark modes.',
             screenShotSmall: 'screenShotSmall',
-            project_background:
-                'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+            project_background: '',
+            dev_with:
+                'Semantic HTML5 markup | CSS custom properties | Flexbox | CSS Grid |  Mobile-first workflow | React | | Styled-Components | Axios | React-route-dom-v6 | React-icons | react-select |',
             liveSite: 'https://glebkas.github.io/countries/',
             github: 'https://github.com/Glebkas/countries',
         },
